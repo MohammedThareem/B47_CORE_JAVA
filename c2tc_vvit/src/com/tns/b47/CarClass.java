41822d548e6b85865e9a5980a2ae7063f438e1da
MohammedThareem <mohammedthareem512@gmail.com>
2023-03-16 17:02:02 +0530

    OOPS COMMIT

---
+++ c2tc_vvit/src/com/tns/b47/CarClass.java
@@ -0,0 +1,17 @@
+package com.tns.b47;
+
+public class CarClass {
+	int model;
+	int reg;
+	String color;
+
+void forward(){
+	System.out.println("Car is moving forward");
+}
+void backward(){
+	System.out.println("Car is moving backward");
+}
+void stop(){
+	System.out.println("Car has stoped moving");
+}
+}
---
+++ c2tc_vvit/src/com/tns/b47/Executor.java
@@ -0,0 +1,15 @@
+package com.tns.b47;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CarClass ob=new CarClass();
+		ob.backward();
+		ob.forward();
+		ob.stop();
+		
+	}
+
+}
+
